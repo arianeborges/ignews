c06dd056f45d561a9b3a93856380249759e9c712
Ariane <ariianeboorges@gmail.com>
2021-05-18 17:35:08 +0100

    Prevent user duplication at stripe customers

--- src/pages/api/subscribe.ts
+++ src/pages/api/subscribe.ts
@@ -1,18 +1,59 @@
+import { query } from 'faunadb';
 import {NextApiRequest, NextApiResponse} from 'next';
 import { getSession } from 'next-auth/client';
+import { fauna } from '../../services/fauna';
 import { stripe } from '../../services/stripe';
+
+type User = {
+   ref: {
+      id: string;
+   },
+   data: {
+      stripe_customer_id: string;
+   }
+}
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
    if(req.method === 'POST'){
       const session = await getSession({req});
 
-      const stripeCustomer = await stripe.customers.create({
-         email: session.user.email,
-         //metadata
-      });
+      // get the user from faunadb by email
+      const user = await fauna.query<User>(
+         query.Get(
+            query.Match(
+               query.Index('user_by_email'),
+               query.Casefold(session.user.email)
+            )
+         )
+      );
+      
+      let customerId = user.data.stripe_customer_id;
 
-      const stripeCheckoutSession = await stripe.checkout.sessions.create({
-         customer: stripeCustomer.id, // customer id in stripe (not in fauna db)
+      if(!customerId) {
+         // this creates a new customer at stripe
+         const stripeCustomer = await stripe.customers.create({
+            email: session.user.email,
+            //metadata
+         });
+   
+         // this prevents user duplicate searching the user in the database first
+         // this updates the user by email adding the stripe customer id in the database
+         await fauna.query(
+            query.Update(
+               query.Ref(query.Collection('users'), user.ref.id),
+               {
+                  data: {
+                     stripe_customer_id: stripeCustomer.id
+                  }
+               }
+            )
+         );
+
+         customerId = stripeCustomer.id;
+      }
+ 
+     const stripeCheckoutSession = await stripe.checkout.sessions.create({
+         customer: customerId, // customer id in stripe (not in fauna db)
          payment_method_types: ['card'],
          billing_address_collection: 'required',
          line_items: [

6bdf2f7a82ea69c0b023f58477df9f205cfe1f2c
Ariane <ariianeboorges@gmail.com>
2021-05-18 19:05:58 +0100

    Verify the events that Stripe sends to the webhook endpoints

--- src/pages/api/webhooks.ts
+++ src/pages/api/webhooks.ts
@@ -1,9 +1,57 @@
 import {NextApiRequest, NextApiResponse} from 'next';
+import { Readable } from 'stream';
+import Stripe from 'stripe';
+import { stripe } from '../../services/stripe';
+
+// function to read all requisitions and change it to strings
+async function buffer(readable:Readable) {
+   const chunks = [];
+
+   for await (const chunk of readable){
+      chunks.push(
+         typeof chunk === 'string' ? Buffer.from(chunk):chunk
+      );
+   }
+
+   return Buffer.concat(chunks);
+}
+
+export const config = {
+   api: {
+      bodyParser: false
+   }
+}
+
+const relevantEvents = new Set([
+   'checkout.session.completed'
+])
 
 // write this cmd on power shell to listen the webhooks
 // stripe listen --forward-to localhost:3000/api/webhooks 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
-   console.log('evento recebido');
+   if(req.method === 'POST'){
+      const buf = await buffer(req);
 
-   res.status(200).json({ok: true})
+      const secret = req.headers['stripe-signature'];
+
+      let event: Stripe.Event;
+
+      try {
+         event = stripe.webhooks.constructEvent(buf, secret, process.env.STRIPE_WEBHOOK_SECRET)
+      } catch (error) {
+         return res.status(400).send(`Webhook error: ${error.message}`);
+      }
+
+      const { type } = event;
+
+      if(relevantEvents.has(type)){
+         console.log('evento recebido', event);
+      }
+
+
+      res.json({received: true})
+   }else{
+      res.setHeader('Allow', 'POST');
+      res.status(405).end('Method not allowed');
+   }
 }

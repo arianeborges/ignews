b11b40a6a566b90d8875e6ec54145dd94ce13ca4
Ariane <ariianeboorges@gmail.com>
2021-05-20 13:07:30 +0100

    Add session callback to return user active subscription

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -11,7 +11,47 @@ export default NextAuth({
       scope: 'read:user'
     }),
   ],
-  callbacks:{
+  callbacks: {
+    async session(session) {
+
+      try {
+        const userActiveSubscription = await fauna.query(
+          query.Get(
+           query.Intersection([
+            query.Match(
+              query.Index('subscription_by_user_ref'),
+              query.Select(
+                'ref',
+                query.Get(
+                  query.Match(
+                    query.Index('user_by_email'),
+                    query.Casefold(session.user.email)
+                  )
+                )
+              )
+            ),
+            query.Match(
+              query.Index('subscription_by_status'),
+              "active"
+            )
+           ])
+          )
+        )
+
+        return {
+          ...session,
+          activeSubscription: userActiveSubscription
+        };
+
+      } catch {
+
+        return {
+          ...session,
+          activeSubscription: null
+        };
+      }
+
+    },
     async signIn(user, account, profile) {
       const { email } = user;
 
@@ -41,8 +81,6 @@ export default NextAuth({
       } catch {
         return false
       }
-
-      
       return true
     },
   }
